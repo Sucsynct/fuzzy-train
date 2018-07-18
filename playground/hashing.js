d1f6961f15e5b28384757ddcf1a2a2cf4429866b
Sucsynct <sucsynct@gmail.com>
2018-07-18 21:35:52 +0200

    Hash user passwords before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,15 +1,15 @@
 // const {SHA256} = require('crypto-js');
-const jwt = require('jsonwebtoken');
+// const jwt = require('jsonwebtoken');
 
-let data = {
-    id: 10
-};
+// let data = {
+//     id: 10
+// };
 
-let token = jwt.sign(data, '123abc');
-console.log(token);
+// let token = jwt.sign(data, '123abc');
+// console.log(token);
 
-let decoded = jwt.verify(token, '123abc');
-console.log('Decoded: \n', decoded);
+// let decoded = jwt.verify(token, '123abc');
+// console.log('Decoded: \n', decoded);
 
 
 // let msg = 'I am user number 3';
@@ -38,3 +38,19 @@ console.log('Decoded: \n', decoded);
 //     console.log('Data integrity compromised');
 // }
 
+
+const bcrypt = require('bcryptjs');
+
+let password = '123abc!';
+
+// bcrypt.genSalt(10, (err, salt) => {
+//     bcrypt.hash(password, salt, (err, hash) => {
+//         console.log(hash);
+//     });
+// });
+
+let hashedPassword = '$2a$10$eYl/X.VeO13zTDuRAPU24ukXo8slwxOFVe4fATLBiaE8Et0.xUUqi';
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+    console.log(res);
+});
